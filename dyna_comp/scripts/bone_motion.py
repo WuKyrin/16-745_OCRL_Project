0014376c77d447abeb03f093085ee00667bf5aaf
liver121888 <liver121888@gmail.com>
2024-04-23 00:57:39 -0400

    Update submodule commit to the working commit.

--- dyna_comp/scripts/bone_motion.py
+++ dyna_comp/scripts/bone_motion.py
@@ -6,11 +6,17 @@ from geometry_msgs.msg import Pose
 import random
 from visualization_msgs.msg import InteractiveMarkerFeedback
 
+from tf2_ros import TransformBroadcaster
+from geometry_msgs.msg import TransformStamped
+
+
 class PosePublisher(Node):
     def __init__(self):
         super().__init__('pose_publisher')
         self.publisher_ = self.create_publisher(InteractiveMarkerFeedback, 'simple_marker/feedback', 10)
-        timer_period = 1  # 5 Hz
+        self.tf_broadcaster = TransformBroadcaster(self)
+        # timer_period = 5
+        timer_period = 0.2  # 5 Hz
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.base_pose = Pose()
         self.base_pose.position.x = -0.4
@@ -26,30 +32,41 @@ class PosePublisher(Node):
         msg.header.stamp.sec = 0
         msg.header.stamp.nanosec = 0
         msg.header.frame_id = 'world'
-        msg.client_id = "/mobile_manipulator"
+        msg.client_id = "/rviz2"
         msg.marker_name = "Goal"
         msg.control_name = ''
         msg.event_type = 2
-        # msg.pose.position.x = self.base_pose.position.x + random.uniform(-0.01, 0.01)
-        # msg.pose.position.y = self.base_pose.position.y + random.uniform(-0.01, 0.01)
-        # msg.pose.position.z = self.base_pose.position.z + random.uniform(-0.01, 0.01)
-        # msg.pose.orientation.x = self.base_pose.orientation.x + random.uniform(-0.05, 0.05)
-        # msg.pose.orientation.y = self.base_pose.orientation.y + random.uniform(-0.05, 0.05)
-        # msg.pose.orientation.z = self.base_pose.orientation.z + random.uniform(-0.05, 0.05)
-        # msg.pose.orientation.w = self.base_pose.orientation.w + random.uniform(-0.05, 0.05)
+        msg.pose.position.x = self.base_pose.position.x + random.uniform(-0.01, 0.01)
+        msg.pose.position.y = self.base_pose.position.y + random.uniform(-0.01, 0.01)
+        msg.pose.position.z = self.base_pose.position.z + random.uniform(-0.01, 0.01)
+        msg.pose.orientation.x = self.base_pose.orientation.x + random.uniform(-0.05, 0.05)
+        msg.pose.orientation.y = self.base_pose.orientation.y + random.uniform(-0.05, 0.05)
+        msg.pose.orientation.z = self.base_pose.orientation.z + random.uniform(-0.05, 0.05)
+        msg.pose.orientation.w = self.base_pose.orientation.w + random.uniform(-0.05, 0.05)
 
-        msg.pose.position.x = self.base_pose.position.x
-        msg.pose.position.y = self.base_pose.position.y
-        msg.pose.position.z = self.base_pose.position.z
-        msg.pose.orientation.x = self.base_pose.orientation.x
-        msg.pose.orientation.y = self.base_pose.orientation.y
-        msg.pose.orientation.z = self.base_pose.orientation.z
-        msg.pose.orientation.w = self.base_pose.orientation.w
+        # msg.pose.position.x = self.base_pose.position.x
+        # msg.pose.position.y = self.base_pose.position.y
+        # msg.pose.position.z = self.base_pose.position.z
+        # msg.pose.orientation.x = self.base_pose.orientation.x
+        # msg.pose.orientation.y = self.base_pose.orientation.y
+        # msg.pose.orientation.z = self.base_pose.orientation.z
+        # msg.pose.orientation.w = self.base_pose.orientation.w
 
         msg.menu_entry_id = 1
         msg.mouse_point_valid = True
         self.publisher_.publish(msg)
         # self.get_logger().info('Publishing: "%s"' % msg)
+
+        # Also publish the transform
+        # t = TransformStamped()
+        # t.header.stamp = msg.header.stamp
+        # t.header.frame_id = 'world'
+        # t.child_frame_id = 'command'
+        # t.transform.translation.x = msg.pose.position.x
+        # t.transform.translation.y = msg.pose.position.y
+        # t.transform.translation.z = msg.pose.position.z
+        # t.transform.rotation = msg.pose.orientation
+        # self.tf_broadcaster.sendTransform(t)
 
 def main(args=None):
     rclpy.init(args=args)
